373a9c30341ebecdf35c17dc94fb5018dd94b734
Mariem <toujanimariam@gmail.com>
2020-11-14 14:23:00 +0100

    Conofig Chrome

--- src/test/java/org/example/Test1.java
+++ src/test/java/org/example/Test1.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -12,24 +13,34 @@ import org.testng.annotations.Test;
 
 public class Test1 {
 
-    WebDriver driver ;
+    WebDriver driver;
+    ChromeOptions options;
 
 
     @BeforeTest
     public void open() {
-        driver =new ChromeDriver();
+        options = new ChromeOptions();
+        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+        driver = new ChromeDriver(options);
+
         driver.get("http://www.google.com");
     }
 
-    @Test(testName="test1",priority = 1)
+    @Test(testName = "test1", priority = 1)
     public void test12() throws Exception {
-        WebElement goo =driver.findElement(By.name("q"));
+        WebElement goo = driver.findElement(By.name("q"));
         goo.sendKeys("test1");
 
     }
-    @Test(testName="test2",priority = 2)
+
+    @Test(testName = "test2", priority = 2)
     public void test22() throws Exception {
-        WebElement goo =driver.findElement(By.name("q"));
+        WebElement goo = driver.findElement(By.name("q"));
         goo.sendKeys("test2");
     }
 
